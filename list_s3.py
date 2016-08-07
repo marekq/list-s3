9343e3609676fdc3945e58fecd97e20440e6ed08
Marek Kuczynski <marek.kuczynski@gmail.com>
2016-08-07 22:17:52 +0200

    cosmetic changes

--- list_s3.py
+++ list_s3.py
@@ -5,18 +5,18 @@
 
 # configure the bucketname to write result CSV file to
 
-bucketn					= 'lists3marek'	
+bucketn	= 'lists3marek'	
 
 # AWS bucket API key and bucket name to write the CSV file with results to - if you leave these blank, the lambda service role will be used which is prefered
-s3acck                  = ''
-s3seck                  = ''
-s3_region_name          = 'eu-west-1'
+s3acck = ''
+s3seck = ''
+s3_region_name = 'eu-west-1'
 
 # AWS account to list the S3 buckets and files in them for - if you leave these blank, the lambda service role will be used which is prefered
 
-aws_acck				= ''
-aws_seck				= ''
-aws_region_name 		= 'eu-west-1'
+aws_acck = ''
+aws_seck = ''
+aws_region_name = 'eu-west-1'
 
 
 ########## ! # ! # don't touch anything below this line # ! # ! ##########
@@ -24,29 +24,29 @@ aws_region_name 		= 'eu-west-1'
 import boto3, time, re, datetime, json, os, sys
 
 # create timestamp format for strftime
-timest					= '%Y-%m-%d_%H-%M'
+timest	= '%Y-%m-%d_%H-%M'
 
 # retrieve metadata about all buckets
 def get_s3(aws_acck, aws_seck, aws_sest, aws_region_name):
 	global resu
-	resu                = []
+	resu = []
 	
-	session             = boto3.session.Session()
+	session = boto3.session.Session()
 
 	if len(aws_seck) == 0 and len(aws_acck) == 0:
-		sts_client      = session.client('s3', region_name = aws_region_name)
+		sts_client = session.client('s3', region_name = aws_region_name)
 		
 	else:
-		sts_client      = session.client('s3', region_name = aws_region_name, aws_access_key_id = aws_acck, aws_secret_access_key = aws_seck, aws_session_token = '')
+		sts_client = session.client('s3', region_name = aws_region_name, aws_access_key_id = aws_acck, aws_secret_access_key = aws_seck, aws_session_token = '')
 		
-		f 				= sts_client.list_buckets()
+		f = sts_client.list_buckets()
 		
 		if f != '':
 			for y in f['Buckets']:
 				try:
-					r		= sts_client.get_bucket_acl(Bucket = y['Name'])
-					owne	= r['Owner']['DisplayName']
-					gran	= r['Grants'][0]['Permission']
+					r    	= sts_client.get_bucket_acl(Bucket = y['Name'])
+					owne 	= r['Owner']['DisplayName']
+					gran 	= r['Grants'][0]['Permission']
 					
 				except Exception as e:
 					owne	= ''
@@ -64,9 +64,9 @@ def get_s3(aws_acck, aws_seck, aws_sest, aws_region_name):
 # get object metadata from the bucket
 def get_s3_obj(sts_client, name, crea, owne, gran, tok):
 	if len(tok) > 0:
-		b 		= sts_client.list_objects_v2(Bucket = name, FetchOwner = True, MaxKeys = 1000, ContinuationToken = tok)
+		b = sts_client.list_objects_v2(Bucket = name, FetchOwner = True, MaxKeys = 1000, ContinuationToken = tok)
 	else:
-		b 		= sts_client.list_objects_v2(Bucket = name, FetchOwner = True, MaxKeys = 1000)
+		b = sts_client.list_objects_v2(Bucket = name, FetchOwner = True, MaxKeys = 1000)
 
 	# capture metadata about objects in bucket
 	if b.has_key('Contents'):
@@ -83,11 +83,11 @@ def get_s3_obj(sts_client, name, crea, owne, gran, tok):
 			reg     = ''
 			
 			# write the results to a list
-			x	    = [name, skey, size, reg, owne, gran, stor, butim, bunix, fitim, funix]
-			y	    = ''
+			x       = [name, skey, size, reg, owne, gran, stor, butim, bunix, fitim, funix]
+			y   	= ''
 			
 			for xx in x:
-				y	+= str(xx)+','
+				y += str(xx)+','
 				
 				resu.append(y[:-1])
 				print 'RUN: '+y[:-1]
@@ -101,7 +101,7 @@ def get_s3_obj(sts_client, name, crea, owne, gran, tok):
 # get s3 website attached to a bucket
 def get_s3_web(sts_client, bucket):
 	try:
-		s3web	= sts_client.get_bucket_website(Bucket = bucket)
+		s3web = sts_client.get_bucket_website(Bucket = bucket)
 		print str(s3web)
 
 	except Exception as e:
@@ -127,15 +127,14 @@ def file_write():
 		put_s3(bucketn, filen)
 
 
-		def put_s3(bucketn, filen):
-			session     = boto3.session.Session()
-			if len(s3acck) == 0 and len(s3seck) == 0:
-				sts_client  = session.client('s3', region_name = s3_region_name)    
-			else:
-				sts_client  = session.client('s3', region_name = s3_region_name, aws_access_key_id = s3acck, aws_secret_access_key = s3seck)
-
-
-				sts_client.upload_file('/tmp/'+filen, 'lists3marek', filen)
+def put_s3(bucketn, filen):
+	session     = boto3.session.Session()
+	if len(s3acck) == 0 and len(s3seck) == 0:
+		sts_client  = session.client('s3', region_name = s3_region_name)    
+	else:
+		sts_client  = session.client('s3', region_name = s3_region_name, aws_access_key_id = s3acck, aws_secret_access_key = s3seck)
+			
+	sts_client.upload_file('/tmp/'+filen, 'lists3marek', filen)
 				
 				
 # # # # #
